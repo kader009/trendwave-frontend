4d75f4a0ad44ef1024f8185e11992f450009e70b
kader009 <kadermollarasel000@gmail.com>
2025-06-09 11:40:14 +0600

    order remove functionality added on the admin panel

--- src/app/dashboard/admin/all-order/page.tsx
+++ src/app/dashboard/admin/all-order/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useAllOrdersQuery } from '@/redux/api/endApi';
+import { useAllOrdersQuery, useDeleteOrderMutation } from '@/redux/api/endApi';
 import { useAppSelector } from '@/redux/hooks';
 import { RootState } from '@/redux/store';
 import Image from 'next/image';
 import React from 'react';
+import { toast } from 'sonner';
 
 interface Orders {
   _id: string;
@@ -15,12 +16,29 @@ interface Orders {
   price: number;
   image: string;
   customerEmail: string;
-  paymentStatus:string
+  paymentStatus: string;
 }
 
 const OrderTable = () => {
   const { user } = useAppSelector((state: RootState) => state.user);
-  const { data: orders, isLoading } = useAllOrdersQuery(user?.email);
+  const {
+    data: orders,
+    isLoading,
+    refetch,
+  } = useAllOrdersQuery(user?.email, { pollingInterval: 2000 });
+
+  const [deleteOrder] = useDeleteOrderMutation();
+
+  const handleRemove = async (id: string) => {
+    try {
+      await deleteOrder(id);
+      toast.success('Order removed successfully');
+      refetch(); // refresh the list
+    } catch (err) {
+      toast.error('Failed to remove order');
+      console.error(err);
+    }
+  };
 
   if (isLoading) return <p className="text-center py-4">Loading...</p>;
 
@@ -59,13 +77,9 @@ const OrderTable = () => {
               </td>
               <td className="px-4 py-2 text-slate-600">{order.category}</td>
               <td className="px-4 py-2">
-                <span className="text-yellow-600 font-semibold">
-                  {order.rating} ⭐
-                </span>
+                <span className=" font-semibold">{order.rating} ⭐</span>
               </td>
-              <td className="px-4 py-2 font-semibold text-green-600">
-                ${order.price}
-              </td>
+              <td className="px-4 py-2 font-semibold ">${order.price}</td>
               <td className="px-4 py-2 text-slate-600">
                 {order.customerEmail}
               </td>
@@ -73,7 +87,10 @@ const OrderTable = () => {
                 {order.paymentStatus}
               </td>
               <td className="px-4 py-2 space-x-2">
-                <button className="text-red-600 hover:underline text-sm">
+                <button
+                  className="text-white hover:underline text-sm bg-black px-3 py-1 rounded-full"
+                  onClick={() => handleRemove(order._id)}
+                >
                   Delete
                 </button>
               </td>
