91493d9aed4917ffcb6e4c75939043a2db0a4f4c
kader009 <kadermollarasel000@gmail.com>
2025-06-11 16:52:53 +0600

    grid layout rearrange for medium size device

--- src/app/flash-sale/page.tsx
+++ src/app/flash-sale/page.tsx
@@ -37,7 +37,7 @@ const FlashSalePage = async () => {
           </div>
 
           {/* Product Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
             {flashData?.FlashSale?.map((product: Product, index: number) => (
               <div
                 key={index}
