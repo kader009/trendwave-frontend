4c31eedd2641d4b87b47d57a07c517435de2bc6a
kader009 <kadermollarasel000@gmail.com>
2025-06-12 10:03:48 +0600

    seller section cancel button bg color update

--- src/app/dashboard/seller/view-all-product/page.tsx
+++ src/app/dashboard/seller/view-all-product/page.tsx
@@ -215,7 +215,7 @@ const ViewProduct = () => {
               <div className="flex justify-end gap-3">
                 <button
                   type="button"
-                  className="px-4 py-2 bg-gray-300 rounded-md text-white"
+                  className="px-4 py-2 bg-black rounded-md text-white"
                   onClick={() => setIsModalOpen(false)}
                 >
                   Cancel
