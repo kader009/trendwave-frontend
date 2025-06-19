99bf426fc7fcd21afff8f96f9aeed3d75d7bb078
kader009 <kadermollarasel000@gmail.com>
2025-06-19 10:52:51 +0600

    image link update from the imgbb

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -7,31 +7,31 @@ const testimonials = [
   {
     id: 1,
     name: 'Sarah Malik',
-    image: 'https://randomuser.me/api/portraits/women/65.jpg',
+    image: 'https://i.ibb.co/twBbb0wG/65.jpg',
     text: 'TrendWave gave me the best shopping experience. Fast delivery and excellent service!',
   },
   {
     id: 2,
     name: 'John Doe',
-    image: 'https://randomuser.me/api/portraits/men/75.jpg',
+    image: 'https://i.ibb.co/8WPRv69/75.jpg',
     text: 'Amazing product quality and great customer support. Highly recommended!',
   },
   {
     id: 3,
     name: 'Anika Rahman',
-    image: 'https://randomuser.me/api/portraits/women/44.jpg',
+    image: 'https://i.ibb.co/hht85r8/44.jpg',
     text: 'I love shopping at TrendWave. Their deals and service are unbeatable.',
   },
   {
     id: 4,
     name: 'James Howard',
-    image: 'https://randomuser.me/api/portraits/men/41.jpg',
+    image: 'https://i.ibb.co/Xf1x8jHF/41.jpg',
     text: 'The checkout process is smooth and fast. Will buy again!',
   },
   {
     id: 5,
     name: 'Maya Hasan',
-    image: 'https://randomuser.me/api/portraits/women/30.jpg',
+    image: 'https://i.ibb.co/QygWy60/31.jpg',
     text: 'Customer service was extremely helpful. Love this store!',
   },
 ];
