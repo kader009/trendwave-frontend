2f19b008eb86d29f5c3761f6824f1d4527bfa4c1
kader009 <kadermollarasel000@gmail.com>
2025-06-30 10:43:28 +0600

    header section folder change and update on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import BrandMarquee from '@/components/BrandPromotion';
 import Testimonial from '@/components/Testimonial';
 import Category from '@/pages/Category';
 import FlashSale from '@/pages/FlashSale';
-import Header from '@/pages/Header';
+import Header from '@/components/Header';
 import TopProducts from '@/pages/TopProducts';
 
 export default function Home() {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
-// Header.tsx
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="relative w-full h-screen bg-[#a98df3] text-white overflow-hidden flex items-center justify-center px-4 sm:px-6 md:px-8">
+    <header className="relative w-full h-screen bg-[#a98df3] text-white overflow-hidden flex items-center justify-center px-4 sm:px-6 md:px-8" aria-label="Main promotional banner">
       <div className="text-center max-w-3xl">
         <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-6xl font-extrabold leading-tight mb-6">
           Discover Style <br /> That Speaks for You
@@ -13,7 +12,7 @@ export default function Header() {
         </p>
         <Link
           href="/products"
-          className="inline-block bg-black text-white hover:bg-gray-600 px-6 py-2 rounded-full font-bold text-lg shadow-md transition-all duration-300"
+          className="inline-block bg-black text-white hover:bg-gray-600 px-6 py-2 rounded-full font-semibold text-lg shadow-md transition-all duration-300"
         >
           Shop Now
         </Link>
