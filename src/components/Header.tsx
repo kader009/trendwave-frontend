54d1eeba16bc3bb6f2895deb9f4dd008fbbefdee
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2025-09-07 16:38:44 +0600

    bg color contrast improve

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,20 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header
-      className="relative w-full h-screen bg-[#a98df3] text-white overflow-hidden flex items-center justify-center px-4 sm:px-6 md:px-8"
+      className="relative w-full h-screen bg-[#6b46c1] text-white overflow-hidden flex items-center justify-center px-4 sm:px-6 md:px-8"
       aria-label="Main promotional banner"
     >
       <div className="text-center max-w-3xl">
         <h1 className="text-5xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold leading-tight mb-6">
           Discover Style <br /> That Speaks for You
         </h1>
-        <p className="text-base sm:text-lg md:text-xl mb-6 text-white/90">
+        <p className="text-base sm:text-lg md:text-xl mb-6 text-white">
           Explore the latest trends in fashion, electronics, and lifestyle with
           good & valuable products.
         </p>
         <Link
           href="/products"
-          className="px-6 py-2 text-xl rounded-full border border-white text-white bg-transparent hover:bg-black hover:text-white transition-colors duration-300 hover:border-none"
+          className="px-6 py-2 text-xl rounded-full border border-white text-white bg-transparent hover:bg-white hover:text-black transition-colors duration-300"
         >
           Shop Now
         </Link>
